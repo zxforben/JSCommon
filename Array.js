9cfee80de5937241c9a40105d876bbf0e9dd0ea8
frankzhangwieswolf <frank.zhang@bleum.com>
2017-05-22 13:49:13 +0800

    add some statements

--- Array.js
+++ Array.js
@@ -4,11 +4,19 @@ var b = new Array(10);
 
 var c  = new Array(5,4,3,2,1,"testing, testing");
 
-//稀疏数组
+//稀疏数组: 数组长度大于元素个数
 a = new Array(5);
 
 a=[];
 
 a[1000]=0;
+
+var a = [1,2,3];
+
+a.join(); // "1,2,3"
+
+a.join(" "); //"1 2 3"
+
+a.join("");  //"123""
 
 
